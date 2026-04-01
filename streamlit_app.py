894dc2eeb5b489317afd355d39e42902e7f1b2f6
gis-carbonx <gis@carbonxco.com>
2026-04-01 11:40:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import folium
-import geopandas as gpd
+import requests
 from streamlit_folium import st_folium
 
 st.title("Simple Map")
@@ -10,5 +10,29 @@ m = folium.Map(
     zoom_start=10,
     tiles="CartoDB positron"
 )
+
+# Load grid GeoJSON from Google Drive
+url = "https://drive.google.com/uc?id=1r19dsxoqU-fY0XvPu1gonn7k1kNA3lyo"
+data = requests.get(url).json()
+
+# Style grid
+def style_function(feature):
+    return {
+        "fillColor": "transparent",
+        "color": "black",  
+        "weight": 1,
+        "fillOpacity": 0  
+    }
+
+folium.GeoJson(
+    data,
+    name="Grid",
+    style_function=style_function,
+    tooltip=folium.GeoJsonTooltip(
+        fields=["Index"],
+        aliases=["Grid ID:"], 
+        sticky=True
+    )
+).add_to(m)
 
 st_folium(m, width=700, height=500)
