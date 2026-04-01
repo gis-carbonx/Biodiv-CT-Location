2dcf658a4bf108ae4a3547871e9a9c37628ec81d
gis-carbonx <gis@carbonxco.com>
2026-04-01 11:09:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import folium
+import geopandas as gpd
+from streamlit_folium import st_folium
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Simple Map")
+
+m = folium.Map(
+    location=[0.7942129043922784, 110.23949401166948],
+    zoom_start=10,
+    tiles="CartoDB positron"
 )
+
+gdf = gpd.read_file("./Data/Grid.json")
+
+folium.GeoJson(gdf, name="Grid").add_to(m)
+
+st_folium(m, width=700, height=500)
